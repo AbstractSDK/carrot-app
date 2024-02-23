616278a7a6e7f3d691aee5b16618fbfbe789fe91
Buckram <buckram123@gmail.com>
2024-02-23 14:35:31 +0200

    fix parsing

--- contracts/carrot-app/src/replies/add_to_position.rs
+++ contracts/carrot-app/src/replies/add_to_position.rs
@@ -1,24 +1,23 @@
 use abstract_app::abstract_sdk::AbstractResponse;
-use cosmwasm_std::{from_json, DepsMut, Env, Reply};
+use cosmwasm_std::{DepsMut, Env, Reply, StdError, SubMsgResponse, SubMsgResult};
 use osmosis_std::types::osmosis::concentratedliquidity::v1beta1::MsgAddToPositionResponse;
 
 use crate::{
-    contract::{App, AppResult},
-    helpers::get_user,
-    state::{Position, POSITION},
+    contract::{App, AppResult}, error::AppError, helpers::get_user, state::{Position, POSITION}
 };
 
 pub fn add_to_position_reply(deps: DepsMut, env: Env, app: App, reply: Reply) -> AppResult {
-    let b = reply
-        .result
-        .unwrap()
-        .data
-        .expect("Failed to add to position");
+    let SubMsgResult::Ok(SubMsgResponse { data: Some(b), .. }) = reply.result else {
+        return Err(AppError::Std(StdError::generic_err(
+            "Failed to create position",
+        )));
+    };
+
     // Parse the msg exec response from the reply
     let parsed = cw_utils::parse_execute_response_data(&b)?;
 
     // Parse the position response from the message
-    let response: MsgAddToPositionResponse = from_json(parsed.data.unwrap_or_default())?;
+    let response: MsgAddToPositionResponse = parsed.data.unwrap_or_default().try_into()?;
 
     // We get the creator of the position
     let creator = get_user(deps.as_ref(), &app)?;
--- contracts/carrot-app/src/replies/create_position.rs
+++ contracts/carrot-app/src/replies/create_position.rs
@@ -1,23 +1,24 @@
 use abstract_app::abstract_sdk::AbstractResponse;
-use cosmwasm_std::{from_json, DepsMut, Env, Reply};
+use cosmwasm_std::{DepsMut, Env, Reply, StdError, SubMsgResponse, SubMsgResult};
 use osmosis_std::types::osmosis::concentratedliquidity::v1beta1::MsgCreatePositionResponse;
 
 use crate::{
     contract::{App, AppResult},
+    error::AppError,
     helpers::get_user,
     state::{Position, POSITION},
 };
 
 pub fn create_position_reply(deps: DepsMut, env: Env, app: App, reply: Reply) -> AppResult {
-    let b = reply
-        .result
-        .unwrap()
-        .data
-        .expect("Failed to create position");
-    // Parse the msg exec response from the reply
-    let parsed = cw_utils::parse_execute_response_data(&b)?;
-    let response: MsgCreatePositionResponse = from_json(parsed.data.unwrap_or_default())?;
+    let SubMsgResult::Ok(SubMsgResponse { data: Some(b), .. }) = reply.result else {
+        return Err(AppError::Std(StdError::generic_err(
+            "Failed to create position",
+        )));
+    };
 
+    let parsed = cw_utils::parse_execute_response_data(&b)?;
+    // Parse create position response
+    let response: MsgCreatePositionResponse = parsed.data.unwrap_or_default().try_into()?;
     // We get the creator of the position
     let creator = get_user(deps.as_ref(), &app)?;
 
