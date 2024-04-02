e5353428db1719aa3f19cb351bf28de85ecadb13
Buckram <buckram123@gmail.com>
2024-04-02 16:29:41 +0300

    clippy

--- contracts/carrot-app/examples/migrate.rs
+++ contracts/carrot-app/examples/migrate.rs
@@ -1,4 +1,4 @@
-use abstract_app::objects::{module::ModuleVersion, AccountId};
+use abstract_app::objects::AccountId;
 use abstract_interface::{AbstractAccount, InstallConfig, ManagerExecFns};
 use abstract_sdk::core::app::BaseMigrateMsg;
 use cosmwasm_std::to_json_binary;
@@ -24,12 +24,12 @@ fn main() -> anyhow::Result<()> {
 
     let abstr = abstract_client::AbstractClient::new(daemon)?;
 
-    // Add your account_id here
-    let account_id_for_migrate = AccountId::local(todo!());
+    // Put your account_id here
+    let account_id_for_migrate = AccountId::local(0);
+
     let account_to_migrate = abstr.account_from(account_id_for_migrate)?;
     let abstr_account: &AbstractAccount<Daemon> = account_to_migrate.as_ref();
-    let mut module_info = AppInterface::<Daemon>::module_info()?;
-    module_info.version = ModuleVersion::Version("0.1.0".to_owned());
+    let module_info = AppInterface::<Daemon>::module_info()?;
     abstr_account.manager.upgrade(vec![(
         module_info,
         Some(to_json_binary(&carrot_app::msg::MigrateMsg {
